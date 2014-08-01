190f8f11288db2ef014e6b4ec03ea5b5e1bb54a1
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-01 13:02:32 +0800

    sync doc

---
+++ doc/guide/index.md
@@ -0,0 +1,46 @@
+## 综述
+
+stepcontrol是一个控制步骤的组件，本身不提供样式。
+
+* 版本：2.0.0
+* 作者：展烨
+* 标签：
+* demo：[http://kg.kissyui.com/stepcontrol/2.0.0/demo/index.html](http://kg.kissyui.com/stepcontrol/2.0.0/demo/index.html)
+
+## 初始化组件
+
+    S.use('kg/stepcontrol/2.0.0/index', function (S, Stepcontrol) {
+         var stepcontrol = new Stepcontrol(config);
+    })
+
+## API说明
+
+
+### config 参数说明
+
+- container: 容器节点（必须）
+- stepCls: 每个步骤的容器class , 默认 .step
+- nextCls: 下一步 按钮的class, 默认.J_NextStep
+- prevCls: 上一步 按钮的class, 默认.J_PrevStep
+- currentStepCls: 当前步骤的class, 默认.current-step
+- completeCls: 已经完成步骤（即当前步骤之前的所有步骤）的class, 默认.complete
+
+
+### ATTRS
+
+- currentStep: 当前步骤的序号
+
+### 方法
+- activeStep(stepNum): 激活第stepNum个步骤，即给其所在的步骤节点的class加入currentStepCls , 之前的步骤节点加入completeCls
+- async(fn): 在某个步骤中异步操作，fn为用户传人的函数，见demo。
+
+### 事件
+-beforeCurrentStepChange
+-afterCurrentStepChange
+
+      stepcontrol.on('beforeCurrentStepChange',function(e){
+        //e.newVal
+        //e.prevVal
+      });
+
+
---
+++ doc/index.js
@@ -0,0 +1,166 @@
+/**
+ * @fileoverview
+ * @author 展烨<zhanye.xyf@taobao.com>
+ * @module stepcontrol
+ **/
+KISSY.add(function (S, Node,Base) {
+
+    var EMPTY = '';
+    var $ = Node.all;
+
+    /**
+     * 
+     * @class Stepcontrol
+     * @constructor
+     * @extends Base
+     */
+    function Stepcontrol(cfg) {
+        var self = this;
+
+        self.container = S.one(cfg.container);
+        self.stepCls = cfg.stepCls || '.step';
+        self.nextCls = cfg.nextCls || '.J_NextStep';
+        self.prevCls = cfg.prevCls || '.J_PrevStep';
+        self.currentStepCls = cfg.currentStepCls || '.current-step';
+        self.completeCls =  cfg.completeCls || '.complete';
+        self.notPassedAttr = cfg.notPassedAttr || 'data-not-passed';
+        self.asyncFlag = false;
+
+        this._init();
+        //调用父类构造函数
+        Stepcontrol.superclass.constructor.call(self, cfg);
+    }
+    S.extend(Stepcontrol, Base, /** @lends Stepcontrol.prototype*/{
+
+      _init: function () {
+
+        var self = this;
+        self.bindEvent();
+      },
+
+      prev: function (tg) {
+
+        var self = this,
+            currentStepNode = tg.parent(self.stepCls),
+            prevStepNode = currentStepNode.prev(self.stepCls);
+
+        if (!prevStepNode) return;
+
+        self.set('currentStep', self.get('currentStep') - 1);
+
+        if(!self.asyncFlag){
+
+          self.activeStep(self.get('currentStep'));
+        }
+
+        self.asyncFlag = false;
+
+      },
+
+      next: function (tg) {
+
+        var self = this,
+            currentStepNode = tg.parent(self.stepCls),
+            nextStepNode = currentStepNode.next(self.stepCls);
+
+        if (!nextStepNode) return;
+
+        self.set('currentStep', self.get('currentStep') + 1);
+
+        S.log('currentStep is:');
+        S.log(self.get('currentStep'));
+
+        if(!self.asyncFlag){
+
+          self.activeStep(self.get('currentStep'));
+        }
+
+        self.asyncFlag = false;
+      },
+
+      //激活某步骤 
+      activeStep: function(stepNum){
+
+        var self = this;
+
+        if(isNaN(stepNum)){
+          return;
+        }
+
+        self.set('currentStep',stepNum);
+        stepNum -= 1;
+
+        var allStepNodes = self.container.all(self.stepCls),
+          targetStepNode = allStepNodes.item(stepNum);
+
+        allStepNodes.each(function(v,k){
+
+          v = S.one(v);
+
+          if(k < stepNum){
+
+            v.removeClass(self.currentStepCls).addClass(self.completeCls);
+          }
+          else if(k == stepNum){
+
+            v.addClass(self.currentStepCls).removeClass(self.completeCls);
+          }
+          else{
+
+            v.removeClass(self.currentStepCls).removeClass(self.completeCls);
+          }
+        });
+
+        if(self.isGotoStepNode){
+
+          self.gotoStepNode(targetStepNode);
+        }
+
+      },
+
+      async: function(fn){
+
+        var self = this;
+        self.asyncFlag = true;
+        fn();
+      },
+
+      gotoStepNode: function(node){
+
+        var top = node.offset().top;
+        S.log('top is');
+        S.log(top);
+        window.scrollTo(0,top);
+      },
+
+      bindEvent: function(){
+
+        var self = this;
+
+        self.container.delegate('click', self.nextCls, function (e) {
+
+          var tg = S.one(e.currentTarget);
+          if(tg.hasAttr(self.notPassedAttr)){
+            return;
+          }
+          self.next(S.one(e.currentTarget));
+        });
+
+        self.container.delegate('click', self.prevCls, function (e) {
+
+          self.prev(S.one(e.currentTarget));
+        });
+
+      }
+
+    }, {ATTRS : /** @lends Stepcontrol*/{
+      currentStep: {
+                     value: 1
+                   }
+    }});
+
+    return Stepcontrol;
+}, {requires:['node', 'base']});
+
+
+
